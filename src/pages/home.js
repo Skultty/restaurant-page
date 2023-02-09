8378ad40d1ac98557882e3f7b6cce67173099c8d
Skultty <samuel.skultety@outlook.sk>
2023-02-09 19:48:42 +0100

    content for the home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,9 +2,11 @@ import { createElement, append, remove, addClass, removeClass } from '../utils.j
 
 const home = () => {
     let home = createElement('div', 'home');
-    let homeText = createElement('p', 'home-text', 'Welcome to our restaurant!');
+    let homeText = createElement('h1', 'home-text', 'Minimalist Restaurant');
+    let introduction = createElement('p', 'introduction', 'Welcome to The Minimalist Restaurant, where less is more...unless you\'re talking about the flavors in our dishes! Our menu is like a crossword puzzle, not too hard but still satisfying. We believe that the key to a great meal is keeping things simple, just like our menu. Get ready for a dining experience that\'s both effortless and delicious!');
 
     append(home, homeText);
+    append(home, introduction);
 
     return home;
 }
